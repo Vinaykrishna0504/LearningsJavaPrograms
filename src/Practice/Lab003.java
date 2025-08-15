9a299600f74ff785c3c87ffc36c9233e14d89c6d
vinay <vinaykrishnakolae@gmail.com>
2025-08-15 23:02:00 +0530

    Updated

--- src/Practice/Lab003.java
+++ src/Practice/Lab003.java
@@ -2,6 +2,41 @@ package Practice;
 
 public class Lab003 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        // Variables = a reusable container for a value
+
+        //Primitive = simple memory stored directly in memory (Stack)
+        //Reference = Stack points to heap
+
+        // Primitive vs Reference
+        // int          string
+        // double       array
+        // char         object
+        // boolean
+
+
+        int age = 30;
+        int year = 2025;
+        //System.out.println("This year is " + year);
+
+        double price = 19.99;
+        double temperature = 25.5;
+        //System.out.println("$" + price) ;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+        //System.out.println(grade);
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        //System.out.println(isStudent);
+
+        String name = "Vinay Krishna";
+        String Car = "Mustang";
+        String colour = "Blue";
+        System.out.println("Your choice is a " + colour + " " + year + " " + Car);
+
+
     }
 }
---
+++ src/Practice/Rectange.java
@@ -0,0 +1,36 @@
+package Practice;
+import java.util.Scanner;
+
+public class Rectange {
+    public static void main(String[] args) {
+
+//        Calculate area of a rectangle
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+
+        System.out.println("The area of the rectangle is " + area + "cm");
+        scanner.close();
+    }
+}
+
+//Either this can be done
+//        System.out.print("Enter the width: ");
+//double width = scanner.nextDouble();
+//
+//        System.out.print("Enter the height: ");
+//double height = scanner.nextDouble();
+//
+//double area = width * height;
+//
+//        System.out.println("The area of the rectangle is " + area + "cm");
